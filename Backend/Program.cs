d3ca8b90e1896b5b1d6d85e24819d78e4e7fbd4f
Brisa Damore <brisadamore17@gmail.com>
2026-07-26 15:04:05 -0300

    agregué azure

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -92,8 +92,8 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowSpecificOrigins",
         builder => builder
-            .WithOrigins("https://deportivo.azurewebsites.net/",
-                    "https://www.deportivo.azurewebsites.net/",
+            .WithOrigins("https://deportivobackend-h9d0hrcph9e7abge.centralus-01.azurewebsites.net/",
+                    "https://www.deportivobackend-h9d0hrcph9e7abge.centralus-01.azurewebsites.net/",
                     "https://localhost:8000",
                     "https://localhost:7190",
                     "https://localhost:5091")
