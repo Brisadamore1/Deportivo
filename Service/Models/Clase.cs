2a27513178168c5ea3f5518eec1392cef0adf73a
Brisa Damore <brisadamore17@gmail.com>
2026-07-29 15:11:34 -0300

    detalles asistencia

--- Service/Models/Clase.cs
+++ Service/Models/Clase.cs
@@ -6,23 +6,26 @@ namespace Service.Models
     {
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Seleccione un día.")]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DayOfWeek DiaSemana { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Ingrese la hora de inicio.")]
         [DataType(DataType.Time)]
         [DisplayFormat(DataFormatString = "{0:hh\\:mm}", ApplyFormatInEditMode = true)]
         public TimeSpan HoraInicio { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Ingrese la hora de finalización.")]
         [DataType(DataType.DateTime)]
         [DisplayFormat(DataFormatString = "{0:hh\\:mm}", ApplyFormatInEditMode = true)]
         public TimeSpan HoraFin { get; set; }
 
-        [Range(1, 100)]
+        [Range(1, 100, ErrorMessage = "El cupo debe ser entre 1 y 100.")]
         public int CupoMaximo { get; set; } 
         public bool Activa { get; set; } //Si ese horario se suspende temporalmente se desactiva.
+
+        [Required(ErrorMessage = "Seleccione una actividad.")]
+        [Range(1, int.MaxValue, ErrorMessage = "Seleccione una actividad.")]
         public int ActividadId { get; set; }
         public Actividad? Actividad { get; set; } 
         public bool IsDeleted { get; set; } = false;
